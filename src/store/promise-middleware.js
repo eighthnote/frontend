4ebeb4d23ad3eefad5fc9de9012a89e5d0a17a67
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-23 13:31:35 -0700

    add promise middleware

---
+++ src/store/promise-middleware.js
@@ -0,0 +1,28 @@
+import { LOAD_START, LOAD_END, ERROR } from '../components/app/reducers';
+
+const isPromise = val => val && typeof val.then === 'function';
+
+export default ({ dispatch }) => next => action => {
+
+  const { type, payload } = action;
+  if(!isPromise(payload)) return next(action);
+
+  dispatch({ type: LOAD_START });
+
+  return payload
+    .then(
+      result => {
+        dispatch({ type: LOAD_END });
+
+        return dispatch({
+          type,
+          payload: result
+        });
+      },
+      err => {
+        dispatch({ type: LOAD_END });
+        dispatch({ type: ERROR, payload: err });
+        throw err;
+      }
+    );
+};
