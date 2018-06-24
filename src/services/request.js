957c7dd9ce1f628e0b01e8773830e459dab583d2
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-24 10:36:07 -0700

    model work

--- src/services/request.js
+++ src/services/request.js
@@ -39,34 +39,55 @@ function request(url, options = {}, data) {
     _id: 'abc',
     firstName: 'Keli',
     lastName: 'Hansen',
+    picture: 'https://scontent-sea1-1.cdninstagram.com/vp/8e2af0c76066ed7cc1fc7f0b001c285d/5BAEC34C/t51.2885-19/s150x150/12960106_547853175394452_1113775208_a.jpg',
     email: 'keli@keli.com',
-    contact: [{ type: 'text', details: '(555) 555-5555', preferred: true }],
-    availabilityNotes: 'most likely to want to go out on Friday evenings',
+    preferredContact: { type: 'text', details: '(555) 555-5555' },
+    additionalContact: [],
+    availability: 'most likely to want to go out on Friday evenings',
     friends: ['123', '456'],
-    offering: [{ description: 'knitting lessons', seenBy: ['123'] }, { description: 'audiobook recommendations', seenBy: [] }],
-    wanting: [{ description: 'gardening advice', seenBy: ['123', '456'] }, { description: 'someone to get Indian food with', seenBy: [] }]
+    offering: [
+      { _id: 1, description: 'knitting lessons', seenBy: ['123'] },
+      { _id: 2, description: 'audiobook recommendations', seenBy: [] }
+    ],
+    wanting: [
+      { _id: 1, description: 'gardening advice', seenBy: ['123', '456'], expires: new Date(2018, 6, 31) },
+      { _id: 2, description: 'someone to get Indian food with', seenBy: [], once: true }
+    ]
   };
 
   const others = [{
     _id: '123',
     firstName: 'Liz',
     lastName: 'M',
+    picture: 'https://scontent-sea1-1.cdninstagram.com/vp/8e2af0c76066ed7cc1fc7f0b001c285d/5BAEC34C/t51.2885-19/s150x150/12960106_547853175394452_1113775208_a.jpg',
     email: 'liz@liz.com',
-    contact: [{ type: 'call', details: '(555) 555-5555', preferred: true }, { type: 'email', details: 'liz@mail.com', preferred: false }],
-    availabilityNotes: 'Sunday afternoons are generally best',
+    preferredContact: { type: 'call', details: '(555) 555-5555' },
+    additionalContact: [{ _id: 1, type: 'email', details: 'liz@mail.com' }],
+    availability: 'Sunday afternoons are generally best',
     friends: ['abc', '456'],
-    offering: [{ description: 'trumpet serenades', seenBy: ['abc'] }],
-    wanting: [{ description: 'people to come to my dance performance', seenBy: ['456'] }, { description: 'a hug', seenBy: ['abc', '456'] }]
+    offering: [
+      { _id: 1, description: 'trumpet serenades', seenBy: ['abc'] }
+    ],
+    wanting: [
+      { _id: 1, description: 'people to come to my dance performance', seenBy: ['456'], expires: new Date(2018, 5, 30) },
+      { _id: 2, description: 'a hug', seenBy: ['abc', '456'] }
+    ]
   }, {
     _id: '456',
     firstName: 'Fran',
     lastName: 'C',
+    picture: 'https://scontent-sea1-1.cdninstagram.com/vp/8e2af0c76066ed7cc1fc7f0b001c285d/5BAEC34C/t51.2885-19/s150x150/12960106_547853175394452_1113775208_a.jpg',
     email: 'fran@fran.com',
-    contact: [{ type: 'call', details: '(555) 555-5555', preferred: true }, { type: 'email', details: 'fran@mail.com', preferred: false }],
-    availabilityNotes: 'I\'m getting over the flu - don\'t give up on me if I\'m too tired to go out!',
+    preferredContact: { type: 'call', details: '(555) 555-5555' },
+    additionalContact: [{ _id: 1, type: 'email', details: 'fran@mail.com' }],
+    availability: 'I\'m getting over the flu - don\'t give up on me if I\'m too tired to go out!',
     friends: ['abc', '123', '456'],
-    offering: [{ description: 'postcards!', seenBy: ['abc', '123'] }],
-    wanting: [{ description: 'someone to plan a backpacking trip with', seenBy: ['abc'] }]
+    offering: [
+      { _id: 1, description: 'postcards!', seenBy: ['abc', '123'] }
+    ],
+    wanting: [
+      { _id: 1, description: 'someone to plan a backpacking trip with', seenBy: ['abc'], once: true, highPriority: true }
+    ]
   }, {
     _id: '789',
     firstName: 'friend of fran',
