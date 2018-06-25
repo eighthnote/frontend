9ed41e1a0cdd7e1c0a3e657a96696fdeef4ef563
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-25 04:34:52 -0700

    follow reducer pattern recommended in Redux docs to load friends

--- src/components/friends/reducers.js
+++ src/components/friends/reducers.js
@@ -1,8 +1,20 @@
-const FRIENDS_LOAD = 'FRIENDS_LOAD';
+import { combineReducers } from 'redux';
 
-export function friends(state = {}, { type, payload }) {
+export const FRIENDS_LOAD = 'FRIENDS_LOAD';
+
+function friendsById(state = {}, { type, payload }) {
   switch(type) {
+    case FRIENDS_LOAD:
+      return payload.reduce((map, friend) => {
+        map[friend._id] = friend;
+        return map;
+      }, {});
     default:
       return state;
   }
 }
+
+export const friends = combineReducers({
+  byId : friendsById,
+  // allIds : allFriends
+});
--- src/components/friends/reducers.test.js
+++ src/components/friends/reducers.test.js
@@ -1,12 +1,19 @@
 import {
-  friends
-
+  friends,
+  FRIENDS_LOAD
 } from './reducers';
 
 describe('friends reducer', () => {
   it('has a default value of an empty object', () => {
     const state = friends(undefined, {});
-    expect(state).toEqual({});
+    expect(state.byId).toEqual({});
   });
   
+  it('loads friends, storing them in an object with ids as keys', () => {
+    const state = friends({}, { 
+      type: FRIENDS_LOAD,
+      payload: [{ _id: '123', name: 'Pilar' }, { _id: '456', name: 'Bailey' }]
+    });
+    expect(state.byId).toEqual({ 123: { _id: '123', name: 'Pilar' }, 456: { _id: '456', name: 'Bailey' } });
+  });
 });
