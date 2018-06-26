58b85ba6701200bd35581bf286aff7f3002bb6cd
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-26 10:56:27 -0700

    get basic user profile rendering

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,40 +1,47 @@
-import React, { PureComponent, Fragment } from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { loadUser } from './actions';
+import { getAccount } from '../auth/reducers';
 import styles from './Profile.css';
+import { getCurrentUser } from './reducers';
 
-export default class Profile extends PureComponent {
+class Profile extends PureComponent {
   static propTypes = {
-    user: PropTypes.object.isRequired,
-    isUser: PropTypes.bool
+    match: PropTypes.object.isRequired,
+    loadUser: PropTypes.func.isRequired,
+    user: PropTypes.object
   };
 
+  componentDidMount() {
+    const { match } = this.props;
+    const id = match.url === '/profile' ? '5b327868cf85ff348f7775e4' : match.params.id;
+    this.props.loadUser(id);
+  }
+
   render() {
-    const { user, isUser = false } = this.props;
-    const { picture, firstName, lastName, preferredContact, additionalContact, availability, offering, wanting } = user;
+    const { user } = this.props;
+
+    if(!user) return null;
+
+    const { firstName, lastName, pictureUrl, availability, contact } = user;
 
     return (
       <section className={styles.profile}>
-        <img src={picture} alt={`profile picture for ${firstName}`}/>
+        <img src={pictureUrl} alt={`profile picture for ${firstName}`}/>
         <h2>{firstName} {lastName}</h2>
-        <h4>preferred contact info:</h4>
-        <span>{preferredContact.type}</span><br/>
-        <span>{preferredContact.details}</span>
-        {!!additionalContact.length &&
-        <Fragment>
-          <h4>additional contact info:</h4>
-          <ul>
-            {
-              additionalContact.map(item => {
-                return <li key={item._id}>
-                  <span>{additionalContact.type}</span>
-                  <span>{additionalContact.details}</span>
-                </li>;
-              })
-            }
-          </ul>
-        </Fragment>}
+        <h4>contact info:</h4>
+        <ul>
+          {
+            contact.map(item => {
+              return <li key={item._id}>
+                {item}
+              </li>;
+            })
+          }
+        </ul>
         <h4>availability notes:</h4>
-        <span>{availability}</span>
+        <span>{availability.days}</span>
         <h3>offering:</h3>
         <ul>
 
@@ -43,3 +50,11 @@ export default class Profile extends PureComponent {
     );
   }
 }
+
+export default connect(
+  state => ({
+    account: getAccount(state),
+    user: getCurrentUser(state)
+  }),
+  { loadUser }
+)(Profile);
