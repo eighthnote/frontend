59e0d93750a9ccfbd6a9134b1ffb908409ea3834
Katlyn Tucker <studioquirk@yahoo.com>
2018-06-26 11:53:30 -0700

    added giving, requesting to profile

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -4,23 +4,25 @@ import { connect } from 'react-redux';
 import { loadUser } from './actions';
 import { getAccount } from '../auth/reducers';
 import styles from './Profile.css';
-import { getCurrentUser } from './reducers';
+import { getCurrentUser, getGivingArray, getRequestingArray } from './reducers';
 
 class Profile extends PureComponent {
   static propTypes = {
     match: PropTypes.object.isRequired,
     loadUser: PropTypes.func.isRequired,
-    user: PropTypes.object
+    user: PropTypes.object,
+    giving: PropTypes.array,
+    requesting: PropTypes.array
   };
 
   componentDidMount() {
     const { match } = this.props;
-    const id = match.url === '/profile' ? '5b327868cf85ff348f7775e4' : match.params.id;
+    const id = match.url === '/profile' ? '5b328493ee12bd61ae53736b' : match.params.id;
     this.props.loadUser(id);
   }
 
   render() {
-    const { user } = this.props;
+    const { user, giving, requesting } = this.props;
 
     if(!user) return null;
 
@@ -42,8 +44,28 @@ class Profile extends PureComponent {
         </ul>
         <h4>availability notes:</h4>
         <span>{availability.days}</span>
-        <h3>offering:</h3>
+        <h3>giving:</h3>
+        
         <ul>
+          {
+            giving.map(item => {
+              return <li key={item._id}>
+                {item.name}
+              </li>;
+            })
+          }
+
+        </ul>
+        <h3>requesting:</h3>
+        
+        <ul>
+          {
+            requesting.map(item => {
+              return <li key={item._id}>
+                {item.name}
+              </li>;
+            })
+          }
 
         </ul>
       </section>
@@ -54,7 +76,9 @@ class Profile extends PureComponent {
 export default connect(
   state => ({
     account: getAccount(state),
-    user: getCurrentUser(state)
+    user: getCurrentUser(state),
+    giving: getGivingArray(state),
+    requesting: getRequestingArray(state)
   }),
   { loadUser }
 )(Profile);
