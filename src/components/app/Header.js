cfda3f1d496bd0e466fa4d0c0d8e5246ab3d1ba4
Katlyn Tucker <studioquirk@yahoo.com>
2018-06-25 17:08:37 -0700

    adding link and tabs for signin page

--- src/components/app/Header.js
+++ src/components/app/Header.js
@@ -1,10 +1,11 @@
-import React, { Component, Fragment } from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { Component } from 'react';
+import {  Link, NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getUserName } from '../auth/reducers';
 import { logout } from '../auth/actions';
 import styles from './Header.css';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 class Header extends Component {
   static propTypes = {
@@ -18,22 +19,27 @@ class Header extends Component {
     return (
       <header className={styles.header}>
         <h1>Together</h1>
-        <nav>
+        <div className="nav">
           <ul>
-            {name ?
-              <Fragment>
-                <li><NavLink to="/feed">Feed</NavLink></li>
-                <li><NavLink to="/profile">{name}</NavLink></li>
-                <li><NavLink to="friends">Friends</NavLink></li>
-                <li><NavLink to="/plans">Plans</NavLink></li>
-                <li><NavLink to="/auth" onClick={logout}>Log Out</NavLink></li>
-              </Fragment>
-              : <Fragment>
-                <li><NavLink to="/auth/signin">Sign In</NavLink></li>
-                <li><NavLink to="/auth/signup">Sign Up</NavLink></li>
-              </Fragment>}
+            <li><NavLink to="/feed">Feed</NavLink></li>
+            <li><NavLink to="/profile">{name}</NavLink></li>
+            <li><NavLink to="friends">Friends</NavLink></li>
+            <li><NavLink to="/plans">Plans</NavLink></li>
+            <li><NavLink to="/auth" onClick={logout}>Log Out</NavLink></li>
           </ul>
-        </nav>
+        </div>
+        <Tabs className="tabs">
+          <TabList>
+            <Tab><Link to="/auth/signin">Sign In</Link></Tab>
+            <Tab><Link to="/auth/signup">Sign Up</Link></Tab>
+          </TabList>
+          <TabPanel>
+            <h2>Content</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>Content</h2>
+          </TabPanel>
+        </Tabs>
       </header>
     );
   }
