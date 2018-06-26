5ff858dc7530ca210247c9d5e20e2ee748ebc329
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-25 17:52:14 -0700

    update user reducers

--- src/components/profile/reducers.js
+++ src/components/profile/reducers.js
@@ -2,14 +2,40 @@ export const USER_LOAD = 'USER_LOAD';
 
 import { LOGOUT } from '../auth/reducers';
 
-export const getUser = state => state.user;
+export const getCurrentUser = state => state.user;
+export const getGiving = state => state.giving;
+export const getGivingArray = state => Object.values(getGiving(state));
+export const getRequesting = state => state.requesting;
+export const getRequestingArray = state => Object.values(getRequesting(state));
 
 export function user(state = null, { type, payload }) {
   switch(type) {
     case USER_LOAD:
-      return payload;
+      return payload.user;
     case LOGOUT:
       return null;
+    default:
+      return state;
+  }
+}
+
+export function giving(state = {}, { type, payload }) {
+  switch(type) {
+    case USER_LOAD:
+      return payload.giving;
+    case LOGOUT:
+      return {};
+    default:
+      return state;
+  }
+}
+
+export function requesting(state = {}, { type, payload }) {
+  switch(type) {
+    case USER_LOAD:
+      return payload.requesting;
+    case LOGOUT:
+      return {};
     default:
       return state;
   }
--- src/components/profile/reducers.test.js
+++ src/components/profile/reducers.test.js
@@ -1,12 +1,28 @@
 import {
   USER_LOAD,
   user,
-  getUser
+  getCurrentUser,
+  giving,
+  getGiving,
+  getGivingArray,
+  requesting,
+  getRequesting,
+  getRequestingArray
 } from './reducers';
 
 import { LOGOUT } from '../auth/reducers';
 
-const userObject = { firstName: 'Jane' };
+const userObject = { 
+  user: { _id: 'a',
+    firstName: 'Keli',
+    lastName: 'Hansen',
+    pictureUrl: 'pix.com',
+    contact: ['(555) 555-5555'],
+    availability: 'Fridays'
+  },
+  giving: { 1: { _id: '1', type: 'giving' }, 3: { _id: '3', type: 'giving' } },
+  requesting: { 2: { _id: '2', type: 'requesting' } }
+};
 
 describe('user reducer', () => {
   it('has a default value of null', () => {
@@ -14,9 +30,9 @@ describe('user reducer', () => {
     expect(state).toBe(null);
   });
 
-  it('loads a user', () => {
+  it('stores a loaded user', () => {
     const state = user(null, { type: USER_LOAD, payload: userObject });
-    expect(state).toBe(userObject);
+    expect(state).toEqual(userObject.user);
   });
 
   it('clears a user on logout', () => {
@@ -25,8 +41,58 @@ describe('user reducer', () => {
   });
 });
 
+describe('giving reducer', () => {
+  it('has a default value of an empty object', () => {
+    const state = giving(undefined, {});
+    expect(state).toEqual({});
+  });
+
+  it('loads giving objects when a user is loaded', () => {
+    const state = giving({}, { type: USER_LOAD, payload: userObject });
+    expect(state).toEqual(userObject.giving);
+  });
+
+  it('clears giving objects on logout', () => {
+    const state = giving(userObject.giving, { type: LOGOUT });
+    expect(state).toEqual({});
+  });
+});
+
+describe('requesting reducer', () => {
+  it('has a default value of an empty object', () => {
+    const state = requesting(undefined, {});
+    expect(state).toEqual({});
+  });
+
+  it('loads requesting objects when a user is loaded', () => {
+    const state = requesting({}, { type: USER_LOAD, payload: userObject });
+    expect(state).toEqual(userObject.requesting);
+  });
+
+  it('clears requesting objects on logout', () => {
+    const state = requesting(userObject.requesting, { type: LOGOUT });
+    expect(state).toEqual({});
+  });
+});
+
 describe('selectors', () => {
   it('gets the current user object', () => {
-    expect(getUser({ user: userObject })).toBe(userObject);
+    expect(getCurrentUser({ user: userObject.user })).toBe(userObject.user);
+  });
+
+  it('gets the giving state', () => {
+    expect(getGiving({ giving: userObject.giving })).toBe(userObject.giving);
+  });
+
+  it('converts the giving object into an array', () => {
+    expect(getGivingArray({ giving: userObject.giving })).toEqual([{ _id: '1', type: 'giving' }, { _id: '3', type: 'giving' }]);
+  });
+
+  it('gets the requesting state', () => {
+    expect(getRequesting({ requesting: userObject.requesting })).toEqual(userObject.requesting);
+  });
+
+  it('converts the requesting object into an array', () => {
+    expect(getRequestingArray({ requesting: userObject.requesting })).toEqual([{ _id: '2', type: 'requesting' }]);
   });
 });
