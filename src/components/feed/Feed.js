e784a74c4d5968ee7d25dcd32a66ffa0f1772e05
Katlyn Tucker <studioquirk@yahoo.com>
2018-06-26 17:09:54 -0700

    Changed name of feed, looking for break problem

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import styles from './Feed.css';
 
 class Feed extends Component {
   render() {
     return (
-      <div>
-        <h1>I am the feed!</h1>
-      </div>
+      <section className={styles.feed}>
+        <p>My feed!</p>
+      </section>
     );
   }
 }
