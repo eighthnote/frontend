5eb40bc2019c0627a824f9de40b0c167496d91a6
Katlyn Tucker <studioquirk@yahoo.com>
2018-06-27 03:20:40 -0700

    divided paragraphs for styling rows

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -5,7 +5,16 @@ class Feed extends Component {
   render() {
     return (
       <section className={styles.feed}>
-        <p>My feed!</p>
+        <h2>My feed!</h2>
+        <div className="plan">
+          <p className="planOne">Plan 1</p>
+          <p className="planTwo">Plan 2</p>
+          <p className="planThree">Plan 3</p>
+          <p className="planFour">Plan 4</p>
+          <p className="planFive">Plan 5</p>
+          <p className="planSix">Plan 6</p>
+          <p className="planSeven">Plan 7</p>
+        </div>
       </section>
     );
   }
