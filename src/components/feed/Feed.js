0add5900aa2bf188cd01b5e4fb93b508266fe478
Katlyn Tucker <studioquirk@yahoo.com>
2018-06-27 13:16:34 -0700

    setting up feed to style with sections and such

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -4,18 +4,107 @@ import styles from './Feed.css';
 class Feed extends Component {
   render() {
     return (
-      <section className={styles.feed}>
+      <div className={styles.feed}>
         <h2>My feed!</h2>
-        <div className="plan">
+        <section className="plan">
           <p className="planOne">Plan 1</p>
+          <section className="starMan">
+            <h3>💖</h3>
+            <ol id="man with star"></ol>
+          </section>
+          <section className="one-on-one">
+            <h3>😂</h3>
+            <ol id="two with hearts"></ol>
+          </section>
+          <section className="group">
+            <h3>🤦</h3>
+            <ol id="group plan"></ol>
+          </section>
           <p className="planTwo">Plan 2</p>
+          <section className="starMan">
+            <h3>💖</h3>
+            <ol id="man with star"></ol>
+          </section>
+          <section className="one-on-one">
+            <h3>😂</h3>
+            <ol id="two with hearts"></ol>
+          </section>
+          <section className="group">
+            <h3>🤦</h3>
+            <ol id="group plan"></ol>
+          </section>
+
           <p className="planThree">Plan 3</p>
+          <section className="starMan">
+            <h3>💖</h3>
+            <ol id="man with star"></ol>
+          </section>
+          <section className="one-on-one">
+            <h3>😂</h3>
+            <ol id="two with hearts"></ol>
+          </section>
+          <section className="group">
+            <h3>🤦</h3>
+            <ol id="group plan"></ol>
+          </section>
+
           <p className="planFour">Plan 4</p>
+          <section className="starMan">
+            <h3>💖</h3>
+            <ol id="man with star"></ol>
+          </section>
+          <section className="one-on-one">
+            <h3>😂</h3>
+            <ol id="two with hearts"></ol>
+          </section>
+          <section className="group">
+            <h3>🤦</h3>
+            <ol id="group plan"></ol>
+          </section>
+
           <p className="planFive">Plan 5</p>
+          <section className="starMan">
+            <h3>💖</h3>
+            <ol id="man with star"></ol>
+          </section>
+          <section className="one-on-one">
+            <h3>😂</h3>
+            <ol id="two with hearts"></ol>
+          </section>
+          <section className="group">
+            <h3>🤦</h3>
+            <ol id="group plan"></ol>
+          </section>
+
           <p className="planSix">Plan 6</p>
+          <section className="starMan">
+            <h3>💖</h3>
+            <ol id="man with star"></ol>
+          </section>
+          <section className="one-on-one">
+            <h3>😂</h3>
+            <ol id="two with hearts"></ol>
+          </section>
+          <section className="group">
+            <h3>🤦</h3>
+            <ol id="group plan"></ol>
+          </section>
+
           <p className="planSeven">Plan 7</p>
-        </div>
-      </section>
+          <section className="starMan">
+            <h3>💖</h3>
+            <ol id="man with star"></ol>
+          </section>
+          <section className="one-on-one">
+            <h3>😂</h3>
+            <ol id="two with hearts"></ol>
+          </section>
+          <section className="group">
+            <h3>🤦</h3>
+            <ol id="group plan"></ol>
+          </section>
+        </section>
+      </div>
     );
   }
 }
