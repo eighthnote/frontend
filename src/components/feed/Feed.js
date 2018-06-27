de645caa11d1be3f2d77110cdcc106638f3fe6b0
Katlyn Tucker <studioquirk@yahoo.com>
2018-06-27 15:28:38 -0700

    added png files and icons to html in react

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -1,108 +1,52 @@
 import React, { Component } from 'react';
 import styles from './Feed.css';
+import starMan from '../images/starMan.png';
+import twoHearts from '../images/twoHearts.png';
+import groupPlan from '../images/groupPlan.png';
+
 
 class Feed extends Component {
   render() {
     return (
       <div className={styles.feed}>
-        <h2>My feed!</h2>
-        <section className="plan">
-          <p className="planOne">Plan 1</p>
-          <section className="starMan">
-            <h3>💖</h3>
-            <ol id="man with star"></ol>
-          </section>
-          <section className="one-on-one">
-            <h3>😂</h3>
-            <ol id="two with hearts"></ol>
-          </section>
-          <section className="group">
-            <h3>🤦</h3>
-            <ol id="group plan"></ol>
-          </section>
-          <p className="planTwo">Plan 2</p>
-          <section className="starMan">
-            <h3>💖</h3>
-            <ol id="man with star"></ol>
-          </section>
-          <section className="one-on-one">
-            <h3>😂</h3>
-            <ol id="two with hearts"></ol>
-          </section>
-          <section className="group">
-            <h3>🤦</h3>
-            <ol id="group plan"></ol>
-          </section>
-
-          <p className="planThree">Plan 3</p>
-          <section className="starMan">
-            <h3>💖</h3>
-            <ol id="man with star"></ol>
-          </section>
-          <section className="one-on-one">
-            <h3>😂</h3>
-            <ol id="two with hearts"></ol>
-          </section>
-          <section className="group">
-            <h3>🤦</h3>
-            <ol id="group plan"></ol>
-          </section>
-
-          <p className="planFour">Plan 4</p>
-          <section className="starMan">
-            <h3>💖</h3>
-            <ol id="man with star"></ol>
-          </section>
-          <section className="one-on-one">
-            <h3>😂</h3>
-            <ol id="two with hearts"></ol>
-          </section>
-          <section className="group">
-            <h3>🤦</h3>
-            <ol id="group plan"></ol>
-          </section>
-
-          <p className="planFive">Plan 5</p>
-          <section className="starMan">
-            <h3>💖</h3>
-            <ol id="man with star"></ol>
-          </section>
-          <section className="one-on-one">
-            <h3>😂</h3>
-            <ol id="two with hearts"></ol>
-          </section>
-          <section className="group">
-            <h3>🤦</h3>
-            <ol id="group plan"></ol>
-          </section>
-
-          <p className="planSix">Plan 6</p>
-          <section className="starMan">
-            <h3>💖</h3>
-            <ol id="man with star"></ol>
-          </section>
-          <section className="one-on-one">
-            <h3>😂</h3>
-            <ol id="two with hearts"></ol>
-          </section>
-          <section className="group">
-            <h3>🤦</h3>
-            <ol id="group plan"></ol>
-          </section>
-
-          <p className="planSeven">Plan 7</p>
-          <section className="starMan">
-            <h3>💖</h3>
-            <ol id="man with star"></ol>
-          </section>
-          <section className="one-on-one">
-            <h3>😂</h3>
-            <ol id="two with hearts"></ol>
-          </section>
-          <section className="group">
-            <h3>🤦</h3>
-            <ol id="group plan"></ol>
-          </section>
+        {/* <h2>My feed!</h2> */}
+        <section className="planOne">
+          <p>Plan 1</p>
+          <img src={starMan} alt="man with star"/>
+          <img src={twoHearts} alt="two people icons sharing a heart"/>
+          <img src={groupPlan} alt="group icon"/>
+        </section>
+        <section className="planTwo">
+          <p>Plan 2</p>
+          <img src={starMan} alt="man with star"/>
+          <img src={twoHearts} alt="two people icons sharing a heart"/>
+          <img src={groupPlan} alt="group icon"/>
+        </section>
+        <section className="planThree">
+          <p>Plan 3</p>
+          <img src={starMan} alt="man with star"/>
+          <img src={twoHearts} alt="two people icons sharing a heart"/>
+          <img src={groupPlan} alt="group icon"/>
+        </section>
+        <section className="planFour">
+          <p>Plan 4</p>
+          <img src={twoHearts} alt="two people icons sharing a heart"/>
+          <img src={groupPlan} alt="group icon"/>
+        </section>
+        <section className="planFive">
+          <p>Plan 5</p>
+          <img src={twoHearts} alt="two people icons sharing a heart"/>
+          <img src={groupPlan} alt="group icon"/>
+        </section>
+        <section className="planSix">
+          <p>Plan 6</p>
+          <img src={twoHearts} alt="two people icons sharing a heart"/>
+          <img src={groupPlan} alt="group icon"/>
+        </section>
+        <section className="planSeven">
+          <p>Plan 7</p>
+          <img src={twoHearts} alt="two people icons sharing a heart"/>
+          <img src={groupPlan} alt="group icon"/>
         </section>
       </div>
     );
