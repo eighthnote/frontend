f8fe413c74373f7330c879b9dd50f4e8fe524b45
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-28 10:02:30 -0700

    make contact form specific

--- src/components/profile/ContactForm.js
+++ src/components/profile/ContactForm.js
@@ -3,19 +3,17 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { updateProfile } from './actions';
 
-class BasicProfileForm extends PureComponent {
+class ContactForm extends PureComponent {
   static propTypes = {
     updateProfile: PropTypes.func.isRequired,
-    label: PropTypes.string.isRequired,
-    stateKey: PropTypes.string.isRequired
   };
 
   state = {
-    [this.props.stateKey]: ''
+    contact: ''
   };
  
   handleChange = ({ target }) => {
-    this.setState({ [this.props.stateKey]: [target.value] });
+    this.setState({ contact: [target.value] });
   };
 
   handleSubmit = event => {
@@ -24,12 +22,12 @@ class BasicProfileForm extends PureComponent {
   };
 
   render() {
-    const { label, stateKey } = this.props;
+    const { contact } = this.state;
 
     return (
       <form onSubmit={this.handleSubmit}>
-        <label>{label}</label>
-        <input type="text" value={this.state[stateKey]} onChange={this.handleChange}/>
+        <label>Enter Your Preferred Contact Info:</label>
+        <input type="text" value={contact} onChange={this.handleChange}/>
         <button type="submit">SAVE</button>
       </form>
     );
@@ -39,4 +37,4 @@ class BasicProfileForm extends PureComponent {
 export default connect(
   null,
   { updateProfile }
-)(BasicProfileForm);
+)(ContactForm);
