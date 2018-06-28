5d93d3a802ec499874210a8e466c1b9a25bce45a
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-27 21:28:01 -0700

    hide forms unless editing

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -17,6 +17,12 @@ class Profile extends PureComponent {
     giving: PropTypes.array,
     requesting: PropTypes.array
   };
+
+  state = {
+    editingPicture: false,
+    editingContact: false,
+    editingAvailability: false
+  };
  
   componentDidMount() {
     this.props.loadFunction();
@@ -24,6 +30,7 @@ class Profile extends PureComponent {
 
   render() {
     const { profile, giving, requesting, isUser } = this.props;
+    const { editingPicture, editingContact, editingAvailability } = this.state;
 
     if(!profile) return null;
 
@@ -34,19 +41,19 @@ class Profile extends PureComponent {
         {pictureUrl ? 
           <img src={pictureUrl} alt={`profile picture for ${firstName}`}/>
           : <div className="filler-image">:)</div>}
-        {isUser && <PictureForm/>}
+        {isUser && editingPicture && <PictureForm/>}
         <h2>{firstName} {lastName}</h2>
         <h4>Best Way to Contact:</h4>
         <ul>
           {!!contact.length && contact.map((item, i) => <li key={i}>{item}</li>)}
         </ul>
-        {isUser && <BasicProfileForm stateKey="contact" label="Enter Your Preferred Contact Info"/>}
+        {isUser && editingContact && <BasicProfileForm stateKey="contact" label="Enter Your Preferred Contact Info"/>}
         <h4>Best Days:</h4>
         <ul>
           {availability && availability.days && availability.days.map((item, i) => <li key={i}>{capitalize(item)}</li>)}
         </ul>
         <p>{availability && availability.notes}</p>
-        {isUser && <AvailabilityForm/>}
+        {isUser && editingAvailability && <AvailabilityForm/>}
         <Shareable isUser={isUser} heading="Giving" shareableType="giving" shareable={giving}/>
         <Shareable isUser={isUser} heading="Requesting" shareableType="requesting" shareable={requesting}/>
       </section>
