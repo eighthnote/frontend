0acd6e19884d6b1ff769b2f8cd53cd8f3a3b85fc
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-27 21:28:13 -0700

    improve delete buttons

--- src/components/profile/Shareable.js
+++ src/components/profile/Shareable.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { addShareable, removeShareable } from './actions';
 import ShareableForm from './ShareableForm';
 import { formatDate } from '../../utils/formatters';
+import styles from './Shareable.css';
 
 class Shareable extends PureComponent {
   static propTypes = {
@@ -15,17 +16,28 @@ class Shareable extends PureComponent {
     isUser: PropTypes.bool
   };
 
+  state = {
+    editing: false
+  };
+
+  handleClick = (id, shareableType) => {
+    if(confirm('Are you sure?')) this.props.removeShareable(id, shareableType);
+  };
+
   render() {
-    const { heading, shareableType, shareable, addShareable, removeShareable, isUser } = this.props;
+    const { heading, shareableType, shareable, addShareable, isUser } = this.props;
+    const { editing } = this.state;
 
     return (
-      <section>
+      <section className={styles.shareable}>
         <h3>{heading}:</h3>
-        {isUser && <ShareableForm shareableType={shareableType} action="ADD" onComplete={addShareable}/>}
+        {isUser && editing && <ShareableForm shareableType={shareableType} action="ADD" onComplete={addShareable}/>}
         <ul>
           {shareable.map(item => (
             <li key={item._id}>
-              {item.name} {item.date && `(by ${formatDate(item.date)})`} {isUser && <button onClick={() => removeShareable(item._id, shareableType)}>&times;</button>}
+              {item.name}
+              {item.date && `(by ${formatDate(item.date)})`}
+              {isUser && <button className="remove" onClick={() => this.handleClick(item._id, shareableType)}>&times;</button>}
             </li>
           ))}
         </ul>
