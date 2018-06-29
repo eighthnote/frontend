6cf5489ee4d730e4abb7567d0722a20311bfe156
Keli Hansen <keli.p.hansen@gmail.com>
2018-06-28 21:39:00 -0700

    style about page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,9 +1,10 @@
 import React, { PureComponent } from 'react';
+import styles from './About.css';
 
 export default class About extends PureComponent {
   render() {
     return (
-      <div className="about">
+      <div className={styles.about}>
         <p>We built Shareable out of a desire for true personal connection.</p>
         <p>Its purpose is to help friends interact in ways that allow us to increase feelings of community and belonging.</p>
         <p>
@@ -19,8 +20,10 @@ export default class About extends PureComponent {
           There are wonderful resources available to help enhance personal happiness.  There are also incredible social platforms that help us communicate with one another.  We feel Shareable brings an opportunity for these to overlap. Spending time with someone who matters to you, knowing you are there for them when they need support, and that they are there for you is the crucial ingredient in the recipe for happiness. Shareable is not only about what we need for our own happiness, but what we each have to give to each other.
         </p>
         <p>
-          Join Shareable and see what you have to gain by sharing what you have to give. You can find us at: 
-          <a href="https://github.com/eighthnote"/>
+          Join Shareable and see what you have to gain by sharing what you have to give.
+        </p>
+        <p> 
+          You can find the people behind Shareable at <a href="https://github.com/eighthnote" target="_blank">https://github.com/eighthnote</a>.
         </p>
       </div>
     );
