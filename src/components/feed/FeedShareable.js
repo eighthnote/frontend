9286eda3397e95bb23a1444aa298fdd5c6510581
Keli Hansen <keli.p.hansen@gmail.com>
2018-07-01 22:45:56 -0700

    change "date" to "expiration" to match schema

--- src/components/feed/FeedShareable.js
+++ src/components/feed/FeedShareable.js
@@ -7,20 +7,20 @@ export default class FeedShareable extends Component {
   static propTypes = {
     name: PropTypes.string.isRequired,
     type: PropTypes.string.isRequired,
-    date: PropTypes.string,
+    expiration: PropTypes.string,
     owner: PropTypes.string.isRequired,
     ownerId: PropTypes.string.isRequired
   };
 
   render() {
-    const { name, date, type, owner, ownerId } = this.props;
+    const { name, expiration, type, owner, ownerId } = this.props;
 
     return (
       <li className={type}>
         <h3 className="owner"><Link to={`/friends/${ownerId}`}>{owner}</Link> is {type}:</h3>
         <div className="content">
           <h3>{name}</h3>
-          {date && <h3 className="feed-date">by {formatDate(date)}</h3>}
+          {expiration && <h3 className="feed-expiration">by {formatDate(expiration)}</h3>}
         </div>
       </li>
     );
